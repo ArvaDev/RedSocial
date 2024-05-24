8576f835294e2dbe33ef1695d4e4efa796543c70
proyecto-clinica <isai123abel@gmail.com>
2024-05-23 21:10:48 -0600

    terminar la base de datos con las entidades

--- Utils/DataBase.sql
+++ Utils/DataBase.sql
@@ -16,3 +16,35 @@ CREATE Table Users  (
     edad SMALLINT UNSIGNED NULL,
     rol ENUM('estudiante', 'docente') NOT NULL DEFAULT 'docente'
 );
+
+/* tabla pivote de publicacion*/
+CREATE TABLE Publicacion (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_usuario BIGINT NOT NULL,
+    content TEXT NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES User(id)
+);
+
+
+/* tabla pivote de las reacciones*/
+CREATE TABLE Reaction (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_usuario BIGINT NOT NULL,
+    id_publicacion BIGINT NOT NULL,
+    post BIGINT NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES User(id),
+    FOREIGN KEY (id_publicacion) REFERENCES Publicacion(id)
+);
+
+
+/* tabla  de las evento*/
+CREATE TABLE Event (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_usuario BIGINT NOT NULL,
+    nombre_evento VARCHAR(100) NOT NULL,
+    direccion VARCHAR(200) NOT NULL,
+    descripcion TEXT NOT NULL,
+    fecha DATE NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES User(id)
+);
+
